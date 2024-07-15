115876528af025e48a2688ef858855dba1c22ccf
Marcus Gibeson <133388744+MarcusGibeson@users.noreply.github.com>
2024-07-15 13:50:23 -0400

    commit

--- src/main/java/marcusgibesonportfolio/portfolio/Populator.java
+++ src/main/java/marcusgibesonportfolio/portfolio/Populator.java
@@ -17,12 +17,48 @@ public class Populator implements CommandLineRunner {
 
     @Override
     public void run(String...args) throws Exception {
-
         //Projects creation
-        Project AsteroidXtreme = new Project("PacMan Xtreme","description here", "IMG URL HERE", "Project Link URL HERE", "GITHUB URL HERE",  )
 
+        //Asteroid Xtreme Project
+        String[] asteroidTechUsed = {"Java", "LibGDX"};
+        String[] asteroidContributors = {"Marcus Gibeson","Joshua Whittington"};
+        String[] asteroidTags = {"Game", "Java", "LibGDX"};
 
+        Project AsteroidXtreme = new Project(
+            "Asteroid Xtreme",
+            "description here", 
+            "IMG URL HERE", 
+            "Project Link URL HERE", 
+            "GITHUB URL HERE",
+            asteroidTechUsed,
+            "2024-04-15",
+            "null",
+            "Incomplete",
+            asteroidContributors,
+            asteroidTags
+        );
 
+        projectService.save(AsteroidXtreme);
 
+        //Pacman Xtreme Project
+        String[] pacmanTechUsed = {"Java", "LibGDX"};
+        String[] pacmanContributors = {"Marcus Gibeson"};
+        String[] pacmanTags = {"Game", "Java", "LibGDX"};
+
+        Project PacmanXtreme = new Project(
+            "Pacman Xtreme",
+            "description here",
+            "IMG URL HERE",
+            "Project Link URL here",
+            "GITHUB URL here",
+            pacmanTechUsed,
+            "2024-04-07",
+            "null",
+            "Incomplete",
+            pacmanContributors,
+            pacmanTags
+        );
+
+        projectService.save(PacmanXtreme);
     }
 }
